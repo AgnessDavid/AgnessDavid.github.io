757353e50ac914427c0932b077b88c3b793e4366
Agness David <119672669+AgnessDavid@users.noreply.github.com>
2025-08-07 13:23:09 +0000

    Ajout du site web complet

---
+++ script.js
@@ -0,0 +1,36 @@
+    var glide = new Glide('#intro', {
+      type: 'carousel',
+      perView: 4,
+      focusAt: 'center',
+      autoplay: 3000, // Ajout de l'autoplay
+      hoverpause: true,
+      breakpoints: {
+        800: {
+          perView: 2
+        },
+        480: {
+          perView: 1
+        }
+      }
+    });
+
+    glide.mount();
+
+
+        var glide = new Glide('#intro1', {
+      type: 'carousel',
+      perView: 4,
+      focusAt: 'center',
+      autoplay: 3000, // Ajout de l'autoplay
+      hoverpause: true,
+      breakpoints: {
+        800: {
+          perView: 2
+        },
+        480: {
+          perView: 1
+        }
+      }
+    });
+
+    glide.mount();
